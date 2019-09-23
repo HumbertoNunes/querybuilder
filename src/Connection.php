12722e1d4d885957e15646c600afc3b91227ef92
Humberto.Souza <humberto.nts@gmail.com>
2019-09-23 16:02:09 -0400

    Corrigido erros da PSR12

--- src/Connection.php
+++ src/Connection.php
@@ -6,10 +6,10 @@ class Connection
 {
     public static function make()
     {
-    	try {
-            return new \PDO('mysql:host=localhost;dbname=estoque','root','');
-    	} catch (\PDOException $e) {
-    		throw new \Exception($e->getMessage());
-    	}
+        try {
+            return new \PDO('mysql:host=localhost;dbname=estoque', 'root', '');
+        } catch (\PDOException $e) {
+            throw new \Exception($e->getMessage());
+        }
     }
 }
--- src/Helpers/Functions.php
+++ src/Helpers/Functions.php
@@ -2,16 +2,17 @@
 
 function classTreatment($className)
 {
-	$className = explode('\\', $className);
-	
-	$className = end($className);
+    $className = explode('\\', $className);
+    
+    $className = end($className);
 
     $className = strtolower($className);
 
     return pluralizeSimpleNouns($className);
 }
 
-function pluralizeSimpleNouns(string $className): string {
+function pluralizeSimpleNouns(string $className): string
+{
 
     $consonantBeforeY = '/[bcdfghjklmnpqrstvwxz]y$/';
     $manyEnds = '/ch$|s$|sh$|x$|z$/';
@@ -19,10 +20,10 @@ function pluralizeSimpleNouns(string $className): string {
 
     if (preg_match($consonantBeforeY, $className)) {
         return strtolower(preg_replace('/y$/', 'ies', $className));
-    }else if (preg_match($manyEnds, $className)) {
+    } elseif (preg_match($manyEnds, $className)) {
         return strtolower("{$className}es");
-    } else if (preg_match($endsOnFOrFe, $className)) {
-        return strtolower(preg_replace('/fe$|f$/','ves', $className));
+    } elseif (preg_match($endsOnFOrFe, $className)) {
+        return strtolower(preg_replace('/fe$|f$/', 'ves', $className));
     } else {
         return strtolower("{$className}s");
     }
