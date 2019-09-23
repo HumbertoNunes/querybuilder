0498857c7e828e1a695d5234147cc7582d1f313c
Humberto.Souza <humberto.nts@gmail.com>
2019-09-23 15:54:51 -0400

    Configuração de conexão padrão

--- src/Connection.php
+++ src/Connection.php
@@ -7,7 +7,7 @@ class Connection
     public static function make()
     {
     	try {
-            return new \PDO('mysql:host=localhost;dbname=estoque','humberto.souza','');
+            return new \PDO('mysql:host=localhost;dbname=estoque','root','');
     	} catch (\PDOException $e) {
     		throw new \Exception($e->getMessage());
     	}
